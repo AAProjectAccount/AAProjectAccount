636c48e42b02b7751e2c05b91a715ab66fbf39ce
AlexanderAlexLiu <34182337+AlexanderAlexLiu@users.noreply.github.com>
2017-12-04 18:18:17 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,2 @@
 let myHeight = document.getElementById('navigationBar').offsetHeight;
-<<<<<<< HEAD
-document.getElementById('main').style.marginTop = myHeight + 10 "px";
-=======
 document.getElementById('main').style.marginTop = myHeight + 10 + "px";
->>>>>>> c7ee76cdf647916fa42494e0561f9c947fc16161
