fa8e65d1f1228e9fddba6e56565ac6e2b4d4038d
Alex <alexander.liu.2003@gmail.com>
2017-12-04 21:13:06 -0500

    added nav bar dropdown java

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,17 @@
 let myHeight = document.getElementById('navigationBar').offsetHeight;
 document.getElementById('main').style.marginTop = myHeight + 10 + "px";
+
+function toggleMenu() {
+    document.getElementById("dropD").classList.toggle("show");
+	console.log("toggle");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var dropD = document.getElementById("dropD");
+      if (dropD.classList.contains('show')) {
+		console.log("removed")
+        dropD.classList.remove('show');
+      }
+  }
+}
